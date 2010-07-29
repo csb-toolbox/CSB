6784100e3051807ea4266637fd8e6cb71e3ab61b
Ivan Kalev <ivan-kalev@users.noreply.github.com>
2010-07-29 06:25:22 +0000

    Added the Prediction fragment classes

--- csb/bio/fragments/__init__.py
+++ csb/bio/fragments/__init__.py
@@ -14,3 +14,20 @@ class FragmentMatch(object):
     def length(self):
         
         return self.qend - self.qstart + 1
+    
+class PredictionContainer(object):
+    
+    def __init__(self, target, isites_prediction, hmm_prediction, combined_prediction):
+        
+        self.target = target
+        
+        self.isites = isites_prediction
+        self.hmm = hmm_prediction
+        self.combined = combined_prediction 
+        
+class Prediction(object):
+    
+    def __init__(self, alignment, coordinates):
+        
+        self.alignment = alignment
+        self.coordinates = coordinates     
