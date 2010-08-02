d11e377b3cc443fd1344c54e03a88df2a312ec00
Klaus Kopec <Klaus Kopec@localhost>
2010-08-02 11:00:35 +0000

    renamed pdb.py to pdb_format.py to resolve import issues. The issues arose when 'import pdb' was called inside csb/bio/io. The expected behaviour would be to import the Python debugger pdb, but instead one gets the PDB Parser module.
    __init__ now contains entries that allow e.g. 'from bio.io import StructureParser' to simplify these imports

--- csb/bio/io/__init__.py
+++ csb/bio/io/__init__.py
@@ -1,2 +1,6 @@
 from hhpred import HHpredOutputParser
 from clans import ClansParser
+from pdb_format import StructureParser, AsyncStructureParser
+
+__all__ = ['HHpredOutputParser', 'ClansParser', 'StructureParser',
+           'AsyncStructureParser']
