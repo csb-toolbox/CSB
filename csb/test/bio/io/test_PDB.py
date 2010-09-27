ca6cf4e80576a49f310986e22b854eff3df1ef28
Ivan Kalev <ivan-kalev@users.noreply.github.com>
2010-09-27 07:29:18 +0000

    Fixed a critical bug introduced with the previous revision

--- csb/test/bio/io/test_PDB.py
+++ csb/test/bio/io/test_PDB.py
@@ -3,22 +3,23 @@ import sys
 import glob
 import unittest
 
-from csb.bio.io import StructureParser
+from csb.bio.io import AsyncStructureParser
 
 
 class PDBEntryTestCase(unittest.TestCase):
     
     def runTest(self):
-        parser = StructureParser(self.file)
+        parser = AsyncStructureParser()
         try:
-            parser.parse_structure()
+            parser.parse_structure(self.file, 10)
         except:
             ex = sys.exc_info()
             ex[1].args = list(ex[1].args) + [self.file]
+            print '\n{0}    {1}'.format(ex[0].__name__, ex[1].args)
             raise ex[0], ex[1], ex[2]
 
 
-def buildSuite(path_mask):
+def buildSuite(path_mask, top=None):
     """
     @param path_mask: search path and file mask for the PDB files
     @type path_mask: str
@@ -26,7 +27,10 @@ def buildSuite(path_mask):
     
     suite = unittest.TestSuite()
     
-    for file in glob.glob(path_mask):
+    for i, file in enumerate(glob.glob(path_mask), start=1):
+        
+        if top and i > top:
+            break
         
         case = PDBEntryTestCase()
         case.file = file
@@ -46,11 +50,14 @@ if __name__ == '__main__':
  
     """.format(os.path.basename(__file__))
     
-    if len(sys.argv) != 2:
+    if len(sys.argv[1:]) not in (1, 2):
         print usage
         sys.exit(1)        
         
-    path = sys.argv[1]    
+    path = sys.argv[1]
+    top = None
+    if len(sys.argv[1:]) == 2:
+        top = int(sys.argv[2])
 
     if not os.path.isdir(os.path.dirname(path)):
         print '\n ERROR: directory not found: {0}\n'.format(os.path.dirname(path))
@@ -60,7 +67,7 @@ if __name__ == '__main__':
     print 'Loading tests...',     
     sys.stdout.flush()
        
-    suite = buildSuite(path)
+    suite = buildSuite(path, top)
 
     print '{0} tests loaded.\n'.format(suite.countTestCases())
     
