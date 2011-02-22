f31402d15ad00c78e1ee449891b9ab3105a6babc
Martin Mechelke <Martin Mechelke@localhost>
2011-02-22 10:20:39 +0000

    Removed legacy code

--- csb/bio/io/whatif.py
+++ csb/bio/io/whatif.py
@@ -22,23 +22,8 @@ class WhatCheckParser(object):
     Simple WhatIf/WhatCheck Summary parser
     """
 
-    def __init__(self, checks = None):
-        self.binary = '/home/mechelke/install/whatcheck/DO_WHATCHECK.COM'
-        if checks is None:
-            self.checks  = ['NAMCHK', 'HNDCHK', 'WGTCHK',
-                            'MISCHK', 'MO2CHK', 'BNDCHK',
-                            'ANGCHK', 'PLNCHK', 'PL2CHK',
-                            'PUCCHK', 'PC2CHK', 'CHICHK',
-                            'RAMCHK', 'C12CHK', 'INOCHK',
-                            'BMPCHK', 'NQACHK', 'FLPCHK',
-                            'ROTCHK', 'BBCCHK', 'HNQCHK',
-                            'BH2CHK', 'BA2CHK', 'SCOLST',
-                            'BVALST', 'ACCLST', 'PDBLST',
-                            'AXACHK', 'H2OCHK', 'H2OCHK',
-                            'H2OHBO']
-        else:
-            self.checks = checks
-
+    def __init__(self, binary = '/home/mechelke/install/whatcheck/DO_WHATCHECK.COM'):
+        self.binary = binary
     
     def parse_summary(self, fn):
         """
@@ -67,15 +52,18 @@ class WhatCheckParser(object):
         f_handler.close()
         return info
 
+    parse = parse_summary
+
     def run(self, pdb_file):
         wd = os.getcwd()
         tmp = tempfile.mkdtemp()
         base = os.path.basename(pdb_file)
         
-        p = subprocess.call('cp %s %s/.' % (os.path.expanduser(pdb_file), tmp), shell = True)
+        p = subprocess.call('cp %s %s/.' %(os.path.expanduser(pdb_file),
+                                           tmp), shell = True)
         os.chdir(tmp)
 
-        p = subprocess.Popen('%s %s' %(self.binary, os.path.join(tmp,base)),
+        p = subprocess.Popen('%s %s' %(self.binary, os.path.join(tmp, base)),
                              stdout=open("/dev/null", "w"),
                              stderr=open("/dev/null", "w"),
                              shell = True)
@@ -87,92 +75,7 @@ class WhatCheckParser(object):
         os.chdir(wd)
         p = subprocess.call('rm -rf %s' %tmp, shell = True)
         return out
-
-
-    def parse_checkdb(self, fn):
-        """
-        @param fn: source whatif file to parse
-        @type fn: str
-
-        """
-
-        x = open(os.path.expanduser(fn)).read().split('CheckID')[1:]
-
-        d = {}
-        
-        
-        for xx in x:
-            dd = {}
-
-            check, ids, data = self.parse_check([s.strip() for s in xx.split('\n')])
-
-            for id, datum in zip(ids,data):
-
-                model, chain, residue = self._parse_id(id)
-                if not model in dd:
-                    dd[model] = {}
-
-                if not chain in dd[model]:
-                    dd[model][chain] = {}
-
-                try:
-                    dd[model][chain][residue] = float(data)
                     
-                except ValueError:
-                    dd[model][chain][residue] = data
-                    
-
-    def parse_check(self, entry):
-
-        id = entry[0][-6:]
-
-        values = {}
-
-        if not id in CHECKIDS:
-            print id
-            return id, values
-
-        i = grep(entry, 'Type')
-
-        t = entry[i][-5:].strip().lower()
-
-        if t == 'text':
-            t = str
-        else:
-            t = eval(t)
-
-        entry = entry[i:]
-
-        i = grep(entry, 'Name')
-
-        if i is None:
-            return id, values
-
-        entry = entry[i:]
-
-        while len(entry) > 1:
-
-            name = entry[0].split(':')[1].strip()
-            value = t(entry[1].split(':')[1].strip())
-            values[name] = value
-
-            i = grep(entry[1:], 'Name')
-
-            if i is None: break
-
-            entry = entry[i+1:]
-
-        return id, values
-
-        
-                    
-    def parse_id(self, id):
-        a,b,c = id.split(';')[:3]
-
-        return int(a), b.strip(), int(c)
-        
-     
-
         
 
         
@@ -185,7 +88,7 @@ if __name__ == "__main__":
     inf = wp.parse_summary(filename)
 
     pdbfile =  '~/data/whatif/2JZC.pdb'
-    pdbfile =  '~/2kd7.pdb'
+#    pdbfile =  '~/2kd7.pdb'
     inf2 = wp.run(pdbfile)
 
 
