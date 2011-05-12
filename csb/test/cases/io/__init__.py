6f56d567ebbccfa7f9e0f171a1bb2316ffff85d7
Ivan Kalev <Ivan Kalev@localhost>
2011-05-12 10:39:51 +0000

    csb.io.tsv:   - fixed a critical bug: table[row, colName] used to return a Table, not a scalar
    csb.test:
      - added  a test suite for csb.io.Table (this covers csb.io.tsv)

--- csb/test/cases/io/__init__.py
+++ csb/test/cases/io/__init__.py
@@ -6,9 +6,143 @@ import os
 import cStringIO
 import types
 import csb.io
+import csb.io.tsv
 import csb.test as test
 
 
+@test.unit
+class TestTable(test.Case):
+
+    def setUp(self):
+        super(TestTable, self).setUp()
+        
+        self.tsv = self.config.getTestFile('csb.tsv')
+        self.table = csb.io.Table.from_tsv(self.tsv)
+        self.t = csb.io.Table.from_table(self.table, data=True)
+        
+    def testIteration(self):
+        
+        data = list(self.table)
+        self.assertEqual(len(data), 3)
+
+    def testDataAccess(self):
+        
+        firstrow = list(self.table)[0]
+        
+        self.assertEqual(firstrow[1], 11.1)    
+        self.assertEqual(firstrow['ID'], 11)
+        self.assertEqual(firstrow['B'], 'Row eleven')
+        
+        self.assertEqual(firstrow.columns, ('ID', 'A', 'B'))
+        self.assertEqual(firstrow.dump('|'), '11|11.1|Row eleven')
+        
+        self.assertRaises(IndexError, lambda:firstrow[4])
+        self.assertRaises(KeyError, lambda:firstrow['Missing'])        
+        
+    def testSQLDataAccess(self):
+
+        reader = self.table.query('SELECT A, ID FROM TSV WHERE ID > ?', [0])
+        data = list(reader)
+        firstrow = data[0]
+                
+        self.assertEqual(len(data), 3)
+        self.assertEqual(firstrow[0], 11.1)
+        self.assertEqual(firstrow[1], 11)
+        
+    def testInsert(self):
+        
+        self.t.insert([333, 333.0, '3 3 3']) 
+        self.assertEquals(self.t[3, 2], '3 3 3')
+        
+        self.assertRaises(Exception, lambda:self.t.insert([1]))
+        
+    def testUpdate(self):
+
+        self.assertRaises(csb.io.tsv.InvalidColumnError, lambda:self.t.where('ID').equals(11).update('Missing', 0))            
+        self.t.where('ID').equals(11).update('ID', 133)
+        self.assertEquals(self.t[0, 'ID'], 133)
+        self.assertEquals(self.t[1, 'ID'], 12)
+                
+        self.assertRaises(csb.io.tsv.InvalidColumnError, lambda:self.t.update('Missing', 0))
+        self.assertRaises(csb.io.tsv.InvalidColumnError, lambda:self.t[0, 'Missing'])        
+        self.t.update('ID', 13)
+        
+        for r in self.t[:, 'ID']:
+            self.assertEquals(r['ID'], 13)                    
+            
+        self.t[0, 'ID'] = 11
+        self.t[1, 'ID'] = 12
+        self.assertEquals(self.t[0, 'ID'], 11)
+        self.assertEquals(self.t[2, 'ID'], 13)        
+    
+    def testDump(self):
+        
+        with self.config.getTempStream() as dump:
+            
+            self.table.dump(dump)
+            dump.flush()
+            
+            self.assertEqual(open(self.tsv).read(), open(dump.name).read())
+            
+    def testScalar(self):
+        
+        self.assertEquals(self.table.scalar(0, 'ID'), 11)
+        self.assertEquals(self.table[0, 'ID'], 11)
+        self.assertEquals(self.table[0, 0], 11)
+        
+        self.assertTrue(isinstance(self.table[:, :], csb.io.Table))
+        self.assertTrue(isinstance(self.table[:, ('A',)], csb.io.Table))
+        self.assertTrue(isinstance(self.table[:, 'A':], csb.io.Table))
+        self.assertTrue(isinstance(self.table[1:2, 0], csb.io.Table))
+        self.assertTrue(isinstance(self.table[(1,2), 0], csb.io.Table))                        
+        
+    def testSelect(self):
+        
+        # test column selection
+        self.assertEquals(self.table.select().columns, ('ID', 'A', 'B'))
+        self.assertEquals(self.table.select('*').columns, ('ID', 'A', 'B'))
+        self.assertEquals(self.table[:, :].columns, ('ID', 'A', 'B'))
+        self.assertEquals(self.table[:, :'A'].columns, ('ID',))
+        self.assertEquals(self.table[:, 'ID':'B'].columns, ('ID', 'A'))                
+                
+        self.assertEquals(self.table.select(['B', 'A']).columns, ('B', 'A'))
+        self.assertEquals(self.table.select('A', 'B').columns, ('A', 'B'))
+        self.assertEquals(self.table[:, ('B', 'A')].columns, ('B', 'A'))
+                
+        self.assertEquals(self.table.select(['A']).columns, ('A',))
+        self.assertEquals(self.table.select(['A']).columns, ('A',))
+        
+        def fr(t):
+            return list(t)[0]
+        
+        # test data        
+        self.assertEquals(len(list(self.table[1:2, :])), 1)
+        self.assertEquals(len(list(self.table[1:, :])), 2)
+        self.assertEquals(len(list(self.table[(1,2), :])), 2)
+        self.assertEquals(len(list(self.table[(0,1,2), :])), 3)                           
+        self.assertEquals(len(list(self.table[:, :])), 3)
+                
+        firstrow = fr(self.table.select('B', 'A'))
+        self.assertEquals(firstrow[0], 'Row eleven')
+        self.assertEquals(firstrow[1], 11.1)
+
+        self.assertEquals(fr(self.table[:, :])[0], 11)
+                
+        self.assertEquals(fr(self.table[:, 'A'])[0], 11.1)
+        self.assertEquals(fr(self.table[:, 'B':])[0], 'Row eleven')
+                
+        self.assertEquals(fr(self.table[2, :])[0], 13)
+        self.assertEquals(fr(self.table[(1,2), :])[0], 12)
+        self.assertEquals(fr(self.table[1:9, :])[0], 12)  
+        
+    def testWhere(self):
+        
+        self.assertEqual(self.table.where('ID').equals(11).select('A').scalar(), 11.1)
+        self.assertEqual(self.table.where('ID').greater(12).select('A').scalar(), 13.3)        
+        self.assertEqual(self.table.where('ID').between(11, 12).select('A').scalar(), 11.1)        
+        self.assertEqual(self.table.where('ID').in_(11).select('A').scalar(), 11.1)
+        self.assertEqual(self.table.where('ID').in_(11, 12).select('A').scalar(), 11.1)                
+        
 @test.unit
 class TestAutoFlushStream(test.Case):
     
