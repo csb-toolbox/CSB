1fbccba62e7b29d792e08caa33351c3101512bd5
Ivan Kalev <ivan-kalev@users.noreply.github.com>
2011-05-16 15:59:40 +0000

    csb.apps:  - buildhmm - CSB app for buildali.pl  - hhsearch - CSB app for HHsearch

---
+++ csb/apps/hhsearch.py
@@ -0,0 +1,161 @@
+"""
+Python bindings for the HHsearch program. Capable of executing multiple
+HHsearch jobs in parallel.
+"""
+
+import multiprocessing as mp
+
+import csb.apps
+import csb.io
+import csb.bio.io
+import csb.pyutils
+
+
+class ExitCodes(csb.apps.ExitCodes):
+    
+    IO_ERROR = 2
+    INVALID_DATA = 3
+    EXT_TOOL_FAILURE = 4
+    
+
+class AppRunner(csb.apps.AppRunner):
+    
+    @property
+    def target(self):
+        return HHsearchApp
+    
+    def command_line(self):
+        
+        cpu = mp.cpu_count()
+        cmd = csb.apps.ArgHandler(self.program, __doc__)
+          
+        cmd.add_scalar_option('binary', 'b', str, 'full path to the HHsearch binary ', default='hhsearch')
+        cmd.add_scalar_option('cpu', 'c', int, 'maximum degree of parallelism', default=cpu)            
+        cmd.add_scalar_option('database', 'd', str, 'the subject (database) HMM file', required=True)     
+        cmd.add_array_argument('query', str, 'query HMM file(s)')      
+        
+        return cmd
+    
+
+class HHsearchApp(csb.apps.Application):
+    
+    def main(self):
+        
+        queries = list(self.args.query)
+        exe = HHsearch(self.args.binary, self.args.database)
+        
+        try:
+            if len(queries) == 1:
+                context = HHTask(queries[0])
+                results = [ exe.run(context, cpu=self.args.cpu) ]
+            else:
+                context = [ HHTask(q for q in queries) ]
+                results = exe.runmany(context, workers=self.args.cpu)
+
+        except IOError as io:
+            HHsearchApp.exit(str(io), ExitCodes.IO_ERROR)
+
+        except csb.pyutils.InvalidCommandError as ose:
+            msg = '{0!s}: {0.cmd}'.format(ose)
+            HHsearchApp.exit(msg, ExitCodes.IO_ERROR)   
+                              
+        except csb.pyutils.ProcessError as pe:
+            message = 'Bad exit code from HHsearch: #{0.code}.\nSTDERR: {0.stderr}\nSTDOUT: {0.stdout}'.format(pe.context)
+            HHsearchApp.exit(message, ExitCodes.EXT_TOOL_FAILURE)
+
+        print '\nRank Hit   Prob  St  End Qst Qend'
+        print '-------------------------------------'
+                            
+        for c in results:
+            print '\n\n# QUERY:{0}\n'.format(c.queryfile)
+            if c.result:
+                for hit in c.result:
+                    print '{0.rank:3}. {0.id:5} {0.probability:5.3f} {0.start:3} {0.end:3} {0.qstart:3} {0.qend:3}'.format(hit)
+
+
+class Context(object):
+    
+    def __init__(self, query):
+        
+        self.__query = query
+        self.__result = None
+    
+    @property
+    def query(self):
+        return self.__query
+    
+    @property
+    def result(self):
+        return self.__result
+    @result.setter
+    def result(self, result):
+        self.__result = result
+    
+
+class HHTask(Context):
+    
+    def __init__(self, queryfile):
+        
+        self.queryfile = queryfile
+        query = open(queryfile).read()
+        
+        super(HHTask, self).__init__(query)
+    
+        
+def _task(args):
+    binary, db, context = args
+    return HHsearch(binary, db).run(context)
+
+    
+class HHsearch(object):
+    
+    def __init__(self, binary, db):
+        
+        self.program = binary
+        self.db = db
+        self.parser = csb.bio.io.HHOutputParser()
+        
+    def run(self, context, cpu=1):
+
+        try:
+            with csb.io.TempFile() as q:
+                
+                q.write(context.query)
+                q.flush()
+                
+                with csb.io.TempFile() as o:
+                    
+                    cmd = '{0.program} -i {1} -d {0.db} -o {2} -cpu {3}'.format(self, q.name, o.name, cpu)
+                    csb.pyutils.Shell.runstrict(cmd)
+                    
+                    context.result = self.parser.parse_file(o.name)
+                    return context
+                
+        except KeyboardInterrupt:
+            return context
+            
+    def runmany(self, contexts, workers=mp.cpu_count()):
+        
+        if workers > len(contexts):
+            workers = len(contexts)
+
+        results = []
+        taskargs = [(self.program, self.db, c) for c in contexts]
+        pool = mp.Pool(workers)
+        
+        try:
+            for c in pool.map(_task, taskargs):
+                results.append(c)
+        except KeyboardInterrupt:
+            pool.terminate()
+        except:
+            pool.terminate()
+            raise
+            
+        return results
+    
+    
+    
+if __name__ == '__main__':
+    
+    AppRunner().run()    
