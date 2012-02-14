7dd159bb6505928bac1e8437831ac1eafd54240f
Ivan Kalev <Ivan Kalev@localhost>
2012-02-14 10:28:27 +0000

    Code formatting

--- csb/test/cases/statistics/pdf/__init__.py
+++ csb/test/cases/statistics/pdf/__init__.py
@@ -24,7 +24,8 @@ class TestParameterEstimation(test.Case):
     def testGeneralizedNormal(self):
         
         mu, alpha, beta = -0.04, 2.11, 1.90
-        data = [-1.1712, -2.5608, -0.7143, 2.6218, -2.0655, 0.7544, 1.208, -0.5289, 0.0045, 1.1746, -1.0766, 1.1198, 1.2785, -0.6051, 2.2913, -3.6672, -0.2525, 0.8782, -0.0617, -0.0239]
+        data = [-1.1712, -2.5608, -0.7143, 2.6218, -2.0655, 0.7544, 1.208, -0.5289, 0.0045, 1.1746, 
+                -1.0766, 1.1198, 1.2785, -0.6051, 2.2913, -3.6672, -0.2525, 0.8782, -0.0617, -0.0239]
         
         pdf = GeneralizedNormal(1, 1, 1)
         pdf.estimate(data)
@@ -56,7 +57,6 @@ class TestParameterEstimation(test.Case):
         self.assertAlmostEqual(pdf.alpha, alpha, places=2)
         self.assertAlmostEqual(pdf.beta, beta, places=1)
 
-
     def testMultivariateGaussian(self):
         d = 3
         mu = numpy.ones(d)
@@ -67,13 +67,13 @@ class TestParameterEstimation(test.Case):
         pdf.estimate(samples)
 
         for i in range(d):
-            self.assertAlmostEqual(pdf.mu[i], mu[i], delta = 1e-1)
+            self.assertAlmostEqual(pdf.mu[i], mu[i], delta=1e-1)
             for j in range(d):
-                self.assertAlmostEqual(pdf.sigma[i,j], sigma[i,j], delta = 1e-1)
+                self.assertAlmostEqual(pdf.sigma[i,j], sigma[i,j], delta=1e-1)
                 
 
         d = 3
-        mu = numpy.array([0.,1.,2.])
+        mu = numpy.array([0., 1., 2.])
         sigma = numpy.random.random((d,d))
         sigma = numpy.dot(sigma, sigma.T)
         pdf = MultivariateGaussian(mu, sigma)
@@ -81,9 +81,9 @@ class TestParameterEstimation(test.Case):
         pdf.estimate(samples)
 
         for i in range(d):
-            self.assertAlmostEqual(pdf.mu[i], mu[i], delta = 1e-1)
+            self.assertAlmostEqual(pdf.mu[i], mu[i], delta=1e-1)
             for j in range(d):
-                self.assertAlmostEqual(pdf.sigma[i,j], sigma[i,j], delta = 1e-1)
+                self.assertAlmostEqual(pdf.sigma[i,j], sigma[i,j], delta=1e-1)
 
         
 
