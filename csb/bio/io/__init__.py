378b5402230286681ad9c40ecaa3f8e57c380eec
Klaus Kopec <Klaus Kopec@localhost>
2012-05-08 15:47:53 +0000

    - big cleanup and refactoring of bio/io/clans.py - added many functional tests for clans.py => 82% of statements covered. This led to necessary changes in the test/data/out.clans file. - added ClansWriter to bio/io/__init__.py

--- csb/bio/io/__init__.py
+++ csb/bio/io/__init__.py
@@ -4,7 +4,7 @@ Biological file format parser classes.
 
 from csb.bio.io.hhpred import HHOutputParser, HHProfileParser
 from csb.bio.io.hhpred import HHpredOutputParser, HHpredProfileParser
-from csb.bio.io.clans import ClansParser
+from csb.bio.io.clans import ClansParser, ClansWriter
 from csb.bio.io.wwpdb import StructureParser, AsyncStructureParser, PDBHeaderParser
 from csb.bio.io.fasta import SequenceParser, PDBSequenceParser
 from csb.bio.io.isites import ISitesParser
