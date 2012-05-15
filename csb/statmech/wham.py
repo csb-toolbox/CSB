51c595e6d3e22bbb6675d8b8caf64baecae4b9f0
Ivan Kalev <ivan-kalev@users.noreply.github.com>
2012-05-15 10:57:08 +0000

    Fixed a number of issues in csb.statmech:  warnings, proper abstract classes, mixed indentation, code formatting

--- csb/statmech/wham.py
+++ csb/statmech/wham.py
@@ -2,19 +2,22 @@
 This module provides algorithms to estimate free energies and density of states
 from tempered ensembles using histogram reweighting
 """
-
 import numpy
-
 
 from csb.math import log, log_sum_exp
 from csb.statistics import histogram_nd
+
+from abc import abstractmethod, ABCMeta
+
 
 class AbstractWHAM(object):
     """
     Abstract base class
     """
+    __metaclass__ = ABCMeta
 
     def __init__(self, ensembles, raw_energies, n):
+        
         self._f = numpy.zeros(len(ensembles))
         self._e = raw_energies
         self._n = n
@@ -22,13 +25,11 @@ class AbstractWHAM(object):
         self._log_g = None
         self._ensembles = ensembles
 
-
-    def log_g(self, normalize = True):
+    def log_g(self, normalize=True):
         """
-        Returns the Density of states (DOS)
+        Return the Density of states (DOS).
 
         @param normalize: Ensure that the density of states sums to one
-
         @rtype float
         """
         if normalize:
@@ -43,9 +44,7 @@ class AbstractWHAM(object):
         """
         return self._f
 
-    
-    
-    def _stop_criterium(self, tol = 1e-10):
+    def _stop_criterium(self, tol=1e-10):
         """
         general stop criterium; if the relative difference between
         sequential negative log likelihoods is less than a predefined
@@ -58,16 +57,18 @@ class AbstractWHAM(object):
         """
         L = self._L
         return  tol is not None and len(L) > 1 and \
-                   abs((L[-2] - L[-1]) /(L[-2] + L[-1])) < tol
+                   abs((L[-2] - L[-1]) / (L[-2] + L[-1])) < tol
         
     
+    @abstractmethod
     def estimate(self, *params):
         """
         Estimate the density of states
         """
         pass
 
-    def log_z(self, beta = 1., ensembles = None):
+    @abstractmethod
+    def log_z(self, beta=1., ensembles=None):
         """
         Compute the partition function for an ensemble at inverse temperature
         beta or for a defined ensemble
@@ -80,12 +81,12 @@ class AbstractWHAM(object):
 
         @rtype: float or array
         """
-        
         pass
+ 
  
 class WHAM(AbstractWHAM):
     """
-    Implementation of the original WHAM methods based on histograms
+    Implementation of the original WHAM methods based on histograms.
     """
 
     def __init__(self, ensembles, raw_energies, n):
@@ -94,10 +95,10 @@ class WHAM(AbstractWHAM):
         self._ex = None
         self._h = None
         
-    def estimate(self, n_bins = 100,  n_iter = 10000, tol = 1e-10):
+    def estimate(self, n_bins=100, n_iter=10000, tol=1e-10):
 
         self._L = []        
-        h, e = histogram_nd(self._e, nbins = n_bins, normalize = False)
+        h, e = histogram_nd(self._e, nbins=n_bins, normalize=False)
         self._ex = e = numpy.array(e)
         self._h = h
         f = self._f
@@ -108,13 +109,13 @@ class WHAM(AbstractWHAM):
         log_n = log(self._n)
 
         e_ij = -numpy.squeeze(numpy.array([ensemble.energy(e)
-                                           for ensemble in self._ensembles] )).T
+                                           for ensemble in self._ensembles])).T
 
         for _i in range(n_iter):
 
             ## update density of states
             y = log_sum_exp(numpy.reshape((e_ij - f + log_n).T,
-                                          (len(f),-1)),0)
+                                          (len(f), -1)), 0)
             log_g = log_h - numpy.reshape(y, log_g.shape)
             log_g -= log_sum_exp(log_g)
 
@@ -131,24 +132,23 @@ class WHAM(AbstractWHAM):
 
         return f, log_g
 
-
-    def log_z(self, beta=1., ensembles = None):
+    def log_z(self, beta=1., ensembles=None):
         """
-        use trapezoidal rule to evaluate the partition function
-        TODO does so far only work for the one 1D
+        Use trapezoidal rule to evaluate the partition function.
+        @todo: does so far only work for the one 1D.
         """
         from numpy import array, multiply, reshape
 
         is_float = False
 
         if type(beta) == float:
-            beta = reshape(array(beta),(-1,))
+            beta = reshape(array(beta), (-1,))
             is_float = True
 
         for i in range(self._ex.shape[0]):
-            x = self._ex[i,1:] - self._ex[i,:-1]
-            y = - multiply.outer(beta, self._ex[i]) + self._log_g
-            y = reshape(array([y.T[1:], y.T[:-1]]), (2, len(beta)*len(x)))
+            x = self._ex[i, 1:] - self._ex[i, :-1]
+            y = -multiply.outer(beta, self._ex[i]) + self._log_g
+            y = reshape(array([y.T[1:], y.T[:-1]]), (2, len(beta) * len(x)))
             y = log_sum_exp(y, 0) - log(2)
             y = reshape(y, (len(x), len(beta))).T + log(x)
 
@@ -159,19 +159,17 @@ class WHAM(AbstractWHAM):
         else:
             return log_z
 
-
-
     
 class ImplicitWHAM(AbstractWHAM):
     """
     Implementation of the implicit WHAM outlined in Habeck 2012, in which histograms
     are reduced to delta peaks, this allows to use energies samples at different orders 
-    of magnitude, improving the accuracy of the DOS estimates
+    of magnitude, improving the accuracy of the DOS estimates.
     """
 
-    def estimate(self, n_iter = 10000, tol = 1e-10):
+    def estimate(self, n_iter=10000, tol=1e-10):
 
-        e_ij =  numpy.array([ensemble.energy(self._e)
+        e_ij = numpy.array([ensemble.energy(self._e)
                              for ensemble in self._ensembles]).T
 
         f = self._f
@@ -180,11 +178,11 @@ class ImplicitWHAM(AbstractWHAM):
         for _i in range(n_iter):
 
             ## update density of states
-            log_g = - log_sum_exp((-e_ij - f + log_n).T, 0)
+            log_g = -log_sum_exp((-e_ij - f + log_n).T, 0)
             log_g -= log_sum_exp(log_g)
 
             ## update free energies            
-            f = log_sum_exp((- e_ij.T + log_g).T, 0)
+            f = log_sum_exp((-e_ij.T + log_g).T, 0)
             self._L.append((self._n * f).sum() - log_g.sum())
 
             self._f = f
@@ -195,37 +193,35 @@ class ImplicitWHAM(AbstractWHAM):
 
         return f, log_g
 
- 
     def log_g(self, normalize=True):
 
-        e_ij = - numpy.array([ensemble.energy(self._e)
+        e_ij = -numpy.array([ensemble.energy(self._e)
                               for ensemble in self._ensembles]).T
         
-        log_g = - e_ij - self._f + log(self._n)
-        log_g = - log_sum_exp(log_g.T, 0)
+        log_g = -e_ij - self._f + log(self._n)
+        log_g = -log_sum_exp(log_g.T, 0)
 
         if normalize:
             log_g -= log_sum_exp(log_g)
 
         return log_g
 
-
-    def log_z(self, beta=1., ensembles = None):
+    def log_z(self, beta=1., ensembles=None):
 
         from numpy import multiply
 
         e_ij = numpy.array([ensemble.energy(self._e)
                               for ensemble in self._ensembles]).T
 
-        x = log_sum_exp((- e_ij - self._f + log(self._n)).T, 0)
+        x = log_sum_exp((-e_ij - self._f + log(self._n)).T, 0)
 
         if ensembles is not None:
             e_ij_prime = numpy.array([ensemble.energy(self._e)
                                       for ensemble in ensembles])
         else:
-            e_ij_prime =  multiply.outer(beta, self._e)
+            e_ij_prime = multiply.outer(beta, self._e)
         
         
-        log_z = log_sum_exp((- e_ij_prime - x).T, 0)
+        log_z = log_sum_exp((-e_ij_prime - x).T, 0)
 
         return log_z
--- csb/test/cases/statmech/ensembles.py
+++ csb/test/cases/statmech/ensembles.py
@@ -1,66 +1,63 @@
 import numpy
-import numpy.random
 
-import csb
+import csb.math
 import csb.test as test
 
-from csb.statmech.ensembles import StatisticalEnsemble, BoltzmannEnsemble, TsallisEnsemble, CompositeEnsemble
+from csb.statmech.ensembles import BoltzmannEnsemble, TsallisEnsemble, CompositeEnsemble
 
 
 @test.functional
-
 class TestEnergy(test.Case):
 
-
     def testBoltzmann(self):
-        e = numpy.linspace(-50,1000, 1000)
+        e = numpy.linspace(-50, 1000, 1000)
 
-        be = BoltzmannEnsemble(beta = 1,)
+        be = BoltzmannEnsemble(beta=1,)
         te = be.energy(e)
         
         for i in range(len(e)):
             self.assertEqual(e[i], te[i])
 
-        be = BoltzmannEnsemble(beta = 0.001,)
+        be = BoltzmannEnsemble(beta=0.001,)
         te = be.energy(e)
         
         for i in range(len(e)):
             self.assertEqual(e[i] * 0.001, te[i])
 
     def testTsallis(self):
-        e = numpy.linspace(-50,1000, 1000)
+        e = numpy.linspace(-50, 1000, 1000)
 
-        tsallis = TsallisEnsemble(q = 1.,)
+        tsallis = TsallisEnsemble(q=1.,)
         te = tsallis.energy(e)
         
         for i in range(len(e)):
             self.assertEqual(e[i], te[i])
 
-        tsallis = TsallisEnsemble(q = 1.1, e_min = -50.)
+        tsallis = TsallisEnsemble(q=1.1, e_min= -50.)
         te = tsallis.energy(e)
         q = 1.1
-        ee = q / (q-1.) * csb.math.log(1 + (q - 1) * (e + 50. )) - 50
+        ee = q / (q - 1.) * csb.math.log(1 + (q - 1) * (e + 50.)) - 50
         
         for i in range(len(e)):
-            self.assertWithinDelta(ee[i], te[i], delta = 1e-5)
+            self.assertWithinDelta(ee[i], te[i], delta=1e-5)
 
 
     def testComposite(self):
-        e1 = numpy.linspace(-50,1000, 1000)
-        e2 = numpy.linspace(-30,3000, 1000)
+        e1 = numpy.linspace(-50, 1000, 1000)
+        e2 = numpy.linspace(-30, 3000, 1000)
 
         q = 1.1
         beta = 0.1
-        ee = q / (q-1.) * csb.math.log(1 + (q - 1) * (e1 + 50. )) - 50
+        ee = q / (q - 1.) * csb.math.log(1 + (q - 1) * (e1 + 50.)) - 50
         ee += e2 * beta
 
-        ce = CompositeEnsemble([TsallisEnsemble(q = q, e_min = -50.),
-                                BoltzmannEnsemble(beta = beta,)])
+        ce = CompositeEnsemble([TsallisEnsemble(q=q, e_min= -50.),
+                                BoltzmannEnsemble(beta=beta,)])
 
-        cee = ce.energy([e1,e2])
+        cee = ce.energy([e1, e2])
         
         for i in range(len(e1)):
-            self.assertWithinDelta(ee[i], cee[i], delta = 1e-5)
+            self.assertWithinDelta(ee[i], cee[i], delta=1e-5)
 
                 
 if __name__ == '__main__':
