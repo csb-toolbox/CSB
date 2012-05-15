27b1e45e30efc8f96b99f75e45d184c2155a6133
Ivan Kalev <ivan-kalev@users.noreply.github.com>
2012-05-15 12:21:53 +0000

    Fixed a bug in Procheck and Whatif: proper use of TempFolder

--- csb/bio/io/procheck.py
+++ csb/bio/io/procheck.py
@@ -30,25 +30,25 @@ class ProcheckParser():
         
         input_file_name = re.compile('>>>-----.*?\n.*?\n\s*\|\s*(\S+)\s+')
         residues = re.compile('(\d+)\s*residues\s\|')
-        ramachandran_plot = re.compile('Ramachandran\splot:\s*(\d+\.\d+)' +
-                                      '%\s*core\s*(\d+\.\d+)%\s*allow\s*(\d+\.\d+)' +
+        ramachandran_plot = re.compile('Ramachandran\splot:\s*(\d+\.\d+)' + 
+                                      '%\s*core\s*(\d+\.\d+)%\s*allow\s*(\d+\.\d+)' + 
                                       '%\s*gener\s*(\d+\.\d+)%\s*disall')
         labelled_all = re.compile('Ramachandrans:\s*(\d+)\s*.*?out\sof\s*(\d+)')
         labelled_chi = re.compile('Chi1-chi2\splots:\s*(\d+)\s*.*?out\sof\s*(\d+)')
         bad_contacts = re.compile('Bad\scontacts:\s*(\d+)')
-        g_factors = re.compile('G-factors\s*Dihedrals:\s*([0-9-+.]+)' +
+        g_factors = re.compile('G-factors\s*Dihedrals:\s*([0-9-+.]+)' + 
                               '\s*Covalent:\s*([0-9-+.]+)\s*Overall:\s*([0-9-+.]+)')
 
-        info['input_file']  = input_file_name.search(text).groups()[0]
-        info['#residues']  = int(residues.search(text).groups()[0])
+        info['input_file'] = input_file_name.search(text).groups()[0]
+        info['#residues'] = int(residues.search(text).groups()[0])
         info['rama_core'], info['rama_allow'], info['rama_gener'], info['rama_disall'] = \
                            [float(g) for g in ramachandran_plot.search(text).groups()]
         info['g_dihedrals'], info['g_bond'], info['g_overall'] = \
                              [float(g) for g in g_factors.search(text).groups()]
-        info['badContacts'] =   int(bad_contacts.search(text).groups()[0])
-        info['labelledAll'] = float(labelled_all.search(text).groups()[0])/ \
+        info['badContacts'] = int(bad_contacts.search(text).groups()[0])
+        info['labelledAll'] = float(labelled_all.search(text).groups()[0]) / \
                               float(labelled_all.search(text).groups()[1])
-        info['labelledChi'] = float(labelled_chi.search(text).groups()[0])/ \
+        info['labelledChi'] = float(labelled_chi.search(text).groups()[0]) / \
                               float(labelled_chi.search(text).groups()[0])
 
         f_handler.close()
@@ -68,14 +68,13 @@ class ProcheckParser():
         base = os.path.basename(pdb_file)
 
         with TempFolder() as tmp:
-            shutil.copy(os.path.expanduser(pdb_file),
-                        tmp)
-            os.chdir(tmp)
-            p = Shell.run('{0} {1} {2}'.format(self.binary,
-                                               os.path.join(tmp, base),
-                                               self.acc))
+            shutil.copy(os.path.expanduser(pdb_file), tmp.name)
+            os.chdir(tmp.name)
+            Shell.run('{0} {1} {2}'.format(self.binary,
+                                           os.path.join(tmp.name, base),
+                                           self.acc))
             summary = '.'.join([os.path.splitext(base)[0], 'sum'])
-            out = self.parse(os.path.join(tmp, summary))
+            out = self.parse(os.path.join(tmp.name, summary))
             os.chdir(wd)
 
         return out
