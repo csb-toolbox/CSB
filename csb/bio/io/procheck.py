62416f8fde10af4e8b53726a381ef0600ecab8b2
Martin Mechelke <Martin Mechelke@localhost>
2012-05-15 11:35:10 +0000

    * Fixes [Issue 0000094]

--- csb/bio/io/procheck.py
+++ csb/bio/io/procheck.py
@@ -3,8 +3,10 @@ Procheck parser
 """
 import os
 import re
-import tempfile
-import subprocess
+import shutil
+
+from csb.pyutils import Shell
+from csb.io import TempFolder
 
 class ProcheckParser():
     """
@@ -14,29 +16,6 @@ class ProcheckParser():
         self.binary = 'procheck.scr'
         self.acc = 2.0
         
-    def run(self, pdb_file):
-
-        wd = os.getcwd()
-        tmp = tempfile.mkdtemp()
-        base = os.path.basename(pdb_file)
-        
-        p = subprocess.call('cp %s %s/.' % (os.path.expanduser(pdb_file), tmp), shell=True)                  
-        os.chdir(tmp)
-
-        p = subprocess.Popen('%s %s %s' %(self.binary, os.path.join(tmp,base), self.acc),
-                             stdout=open("/dev/null", "w"),
-                             stderr=open("/dev/null", "w"),
-                             shell = True)
-        
-        sts = os.waitpid(p.pid, 0)[1]                                                                        
-        summary = '.'.join([os.path.splitext(base)[0],'sum'])
-        out = self.parse(os.path.join(tmp,summary))
-        
-        os.chdir(wd)
-        p = subprocess.call('rm -rf %s' %tmp, shell = True)
-        return out
-
-    
     def parse(self, fn):
         """
         @param fn: source  file to parse
@@ -49,30 +28,54 @@ class ProcheckParser():
         f_handler = open(os.path.expanduser(fn))
         text = f_handler.read()
         
-        inputFileName = re.compile('>>>-----.*?\n.*?\n\s*\|\s*(\S+)\s+')
+        input_file_name = re.compile('>>>-----.*?\n.*?\n\s*\|\s*(\S+)\s+')
         residues = re.compile('(\d+)\s*residues\s\|')
-        ramachandranPlot = re.compile('Ramachandran\splot:\s*(\d+\.\d+)' +
+        ramachandran_plot = re.compile('Ramachandran\splot:\s*(\d+\.\d+)' +
                                       '%\s*core\s*(\d+\.\d+)%\s*allow\s*(\d+\.\d+)' +
                                       '%\s*gener\s*(\d+\.\d+)%\s*disall')
-        labelledAll = re.compile('Ramachandrans:\s*(\d+)\s*.*?out\sof\s*(\d+)')
-        labelledChi = re.compile('Chi1-chi2\splots:\s*(\d+)\s*.*?out\sof\s*(\d+)')
-        badContacts = re.compile('Bad\scontacts:\s*(\d+)')
-        gFactors = re.compile('G-factors\s*Dihedrals:\s*([0-9-+.]+)' +
+        labelled_all = re.compile('Ramachandrans:\s*(\d+)\s*.*?out\sof\s*(\d+)')
+        labelled_chi = re.compile('Chi1-chi2\splots:\s*(\d+)\s*.*?out\sof\s*(\d+)')
+        bad_contacts = re.compile('Bad\scontacts:\s*(\d+)')
+        g_factors = re.compile('G-factors\s*Dihedrals:\s*([0-9-+.]+)' +
                               '\s*Covalent:\s*([0-9-+.]+)\s*Overall:\s*([0-9-+.]+)')
 
-        info['input_file']  = inputFileName.search(text).groups()[0]
+        info['input_file']  = input_file_name.search(text).groups()[0]
         info['#residues']  = int(residues.search(text).groups()[0])
-        info['rama_core'], info['rama_allow'], info['rama_gener'], info['rama_disall'] =\
-                           [float(g) for g in ramachandranPlot.search(text).groups()]
-        info['g_dihedrals'], info['g_bond'], info['g_overall'] =\
-                             [float(g) for g in gFactors.search(text).groups()]
-        info['badContacts'] =   int(badContacts.search(text).groups()[0])
-        info['labelledAll'] = float(labelledAll.search(text).groups()[0])/ \
-                              float(labelledAll.search(text).groups()[1])
-        info['labelledChi'] = float(labelledChi.search(text).groups()[0])/ \
-                              float(labelledChi.search(text).groups()[0])
+        info['rama_core'], info['rama_allow'], info['rama_gener'], info['rama_disall'] = \
+                           [float(g) for g in ramachandran_plot.search(text).groups()]
+        info['g_dihedrals'], info['g_bond'], info['g_overall'] = \
+                             [float(g) for g in g_factors.search(text).groups()]
+        info['badContacts'] =   int(bad_contacts.search(text).groups()[0])
+        info['labelledAll'] = float(labelled_all.search(text).groups()[0])/ \
+                              float(labelled_all.search(text).groups()[1])
+        info['labelledChi'] = float(labelled_chi.search(text).groups()[0])/ \
+                              float(labelled_chi.search(text).groups()[0])
 
         f_handler.close()
         
         return info
 
+
+    def run(self, pdb_file):
+        """
+        Runs procheck for the given pdbfile and parses the output.
+        Will fail if the procheck binary is not in the path.
+        
+        @param pdb_file: file to parse
+        @return: dict of parsed values
+        """
+        wd = os.getcwd()
+        base = os.path.basename(pdb_file)
+
+        with TempFolder() as tmp:
+            shutil.copy(os.path.expanduser(pdb_file),
+                        tmp)
+            os.chdir(tmp)
+            p = Shell.run('{0} {1} {2}'.format(self.binary,
+                                               os.path.join(tmp, base),
+                                               self.acc))
+            summary = '.'.join([os.path.splitext(base)[0], 'sum'])
+            out = self.parse(os.path.join(tmp, summary))
+            os.chdir(wd)
+
+        return out
