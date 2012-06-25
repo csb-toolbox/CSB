ad29b657468c55e2cfff6de0ce00bb2d55af6656
Klaus Kopec <klauskopec@users.noreply.github.com>
2012-06-25 10:44:35 +0000

    refactored ClansWriter, which serialized as a side-effect during instantiation, to the Builder-pattern-based ClansFileWriter.
    - ClansWriter bio.io.__init__ import was changed to ClansFileWriter.
    - ClansWriter test class name updated to ClansFileWriter.

--- csb/bio/io/__init__.py
+++ csb/bio/io/__init__.py
@@ -4,7 +4,7 @@ I/O for various biological file formats.
 
 from csb.bio.io.hhpred import HHOutputParser, HHProfileParser
 from csb.bio.io.hhpred import HHpredOutputParser, HHpredProfileParser
-from csb.bio.io.clans import ClansParser, ClansWriter
+from csb.bio.io.clans import ClansParser, ClansFileWriter
 from csb.bio.io.wwpdb import StructureParser, AsyncStructureParser, PDBHeaderParser
 from csb.bio.io.fasta import SequenceParser, PDBSequenceParser
 from csb.bio.io.isites import ISitesParser
