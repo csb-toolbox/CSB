369815df2b2f8b24ee7f320045c6b67bbd23de28
simeoncarstens <simeoncarstens@users.noreply.github.com>
2012-09-26 17:04:12 +0200

    Improved unit tests for samplers and fixed two minor bugs.
    The unit tests for the ExchangeMC-derived classes in
    test.cases.statistics.samplers have been greatly improved in terms of
    actually failing if the algorithms don't work. Before, the target density
    was just a normal distribution which does not require advanced
    algorithms to sample from. It has now been replaced with a simple
    multimodal distribution, which for low temperatures can only be
    correctly sampled when employing some more advanced algorithm, e.g.
    Replica Exchange. Sampling is assessed by comparing well occupation
    numbers.
    Also, for the HMCSampler class, there is now a test for a non-unity
    mass matrix.
    The tests may take a little longer now, but I prefer that over not noticing
    bugs.
    
    In the AlternatingAdjacentSwapScheme class of statistics.samplers.mc,
    there was a minor bug: if there are only two replicae, a swap was attempted
    only every second time swap_all() was called. This is fixed now.
    
    In ThermostattedMDPropagator of statistics.samplers.mc.propagators,
    an estimate of the number of random numbers to be drawn is calculated.
    For very short trajectory lengths (like up to 5 steps), this number
    could become negative. It is now set to an arbitrary minimum of 5.

--- csb/test/cases/statistics/samplers/__init__.py
+++ csb/test/cases/statistics/samplers/__init__.py
@@ -1,12 +1,13 @@
 import numpy as np
 import csb.test as test
 
-from csb.statistics.pdf import Normal
+from csb.statistics.pdf import Normal, AbstractDensity
 
 from csb.numeric.integrators import AbstractGradient, VelocityVerlet, LeapFrog, FastLeapFrog
 
 from csb.statistics.samplers import State
 from csb.statistics.samplers.mc import MDRENSSwapParameterInfo, ThermostattedMDRENSSwapParameterInfo
+from csb.statistics.samplers.mc import RESwapParameterInfo
 from csb.statistics.samplers.mc import AlternatingAdjacentSwapScheme
 from csb.statistics.samplers.mc.singlechain import HMCSampler, RWMCSampler
 from csb.statistics.samplers.mc.multichain import ReplicaExchangeMC, MDRENS, ThermostattedMDRENS
@@ -17,6 +18,15 @@ class SamplePDF(Normal):
     
     def log_prob(self, x):
         return sum(map(super(SamplePDF, self).log_prob, x))
+
+class MultimodalPDF(AbstractDensity):
+
+    def log_prob(self, x):
+        return sum(-2.5 * np.cos(2.5 * x) - 0.04 * x ** 2)
+
+    def grad(self, x, t):
+        return -6.25 * np.sin(2.5 * x) + 0.08 * x
+
     
 @test.functional
 class TestMCPropagators(test.Case):
@@ -30,7 +40,7 @@ class TestMCPropagators(test.Case):
         self.timestep = 1.2
         self.stepsize = 1.2
         self.nsteps = 25
-        self.nits = 15000
+        self.nits = 10000
         self.state = State(np.random.normal(size=1))
 
     def _createGradient(self, sigma):
@@ -38,15 +48,15 @@ class TestMCPropagators(test.Case):
         class Grad(AbstractGradient):
             def evaluate(self, q, t):
                 return q / (sigma ** 2)
-            
         return Grad()       
 
     def check_result(self, trajectory):
 
-        states = [state.position[0] for state in trajectory]
-        
-        self.assertAlmostEqual(np.array(states).mean(), 0., delta=1e-1)
-        self.assertAlmostEqual(np.array(states).var(), 1., delta=1e-1)
+        dim = len(trajectory[0].position)
+        for i in range(dim):
+            states = [state.position[i] for state in trajectory]
+            self.assertAlmostEqual(np.array(states).mean(), 0., delta=1e-1)
+            self.assertAlmostEqual(np.array(states).var(), 1., delta=1e-1)
 
     def testRWMCPropagator(self):
 
@@ -59,6 +69,14 @@ class TestMCPropagators(test.Case):
         gen = HMCPropagator(self.pdf, self.gradient, self.timestep, self.nsteps)
 
         self.check_result(gen.generate(self.state, self.nits))
+
+    def testHMCPropagator_mm(self):
+
+        mm = np.array([[1., 0.], [0., 2.]])
+        init_state = State(np.random.normal(size=2))
+        gen = HMCPropagator(self.pdf, self.gradient, self.timestep, self.nsteps)
+        self.check_result(gen.generate(init_state, self.nits))
+        
         
 @test.functional
 class TestMultichain(test.Case):
@@ -67,72 +85,104 @@ class TestMultichain(test.Case):
         
         super(TestMultichain, self).setUp()
 
-        self.sigma1, self.sigma2, self.sigma3 = 1., 1. / np.sqrt(3), 1. / np.sqrt(5)
-        init_state = State(np.random.normal(size=2))
+        self.temperatures = [0.4, 2.0]
 
-        self.samplers = [RWMCSampler(SamplePDF(sigma=self.sigma1), init_state, 2),
-                         RWMCSampler(SamplePDF(sigma=self.sigma1), init_state, 1.5),
-                         RWMCSampler(SamplePDF(sigma=self.sigma1), init_state, 1.0)]
+        init_state = State(np.random.uniform(low=-3.0, high=3.0, size=1))
         
-        self.timesteps = [0.05, 0.05]
-        self.nsteps = [100, 100]
-        self.nits = 5000
+        self.samplers = [RWMCSampler(MultimodalPDF(), init_state, 0.5, 
+                                     temperature=self.temperatures[0]),
+                         RWMCSampler(MultimodalPDF(), init_state, 5.5, 
+                                     temperature=self.temperatures[1])]
+        
+        self.nits = 10000
         self.algorithm = None
-        self.temperatures = [lambda l: 1., lambda l: 1.]
+        self.Ts = [lambda l: l * self.temperatures[1] + (1. - l) * self.temperatures[0]]
+
+        self.grad = self.samplers[0]._pdf.grad
+
+    def set_1p_samplerparams(self):
+        init_state = State(np.random.uniform(low=-3.0, high=3.0, size=1))
+        self.samplers[0].stepsize = 0.5
+        self.samplers[1].stepsize = 5.5
+        self.samplers[0].state = init_state
+        self.samplers[1].state = init_state
+
+    def set_2p_samplerparams(self):
+        init_state = State(np.random.uniform(low=-3.0, high=3.0, size=2))
+        self.samplers[0].stepsize = 0.3
+        self.samplers[1].stepsize = 2.5
+        self.samplers[0].state = init_state
+        self.samplers[1].state = init_state
         
-        self.gradients = [self._createGradientL(self.sigma1, self.sigma2),
-                          self._createGradientL(self.sigma2, self.sigma3)]
-
-        mm = np.array([[1., 0.], [0., 2.]])
-        
-        self.params = [ThermostattedMDRENSSwapParameterInfo(self.samplers[0], self.samplers[1],
-                                                            self.timesteps[0], self.nsteps[0],
-                                                            self.gradients[0], mass_matrix=mm),
-                       ThermostattedMDRENSSwapParameterInfo(self.samplers[1], self.samplers[2],
-                                                            self.timesteps[1], self.nsteps[1],
-                                                            self.gradients[1], mass_matrix=mm)]
-
-    def _createGradientL(self, sigma1, sigma2):
-        
-        class Grad(AbstractGradient):
-            def evaluate(self, q, l):
-                return q * (l / (sigma2 ** 2) + (1. - l) / (sigma1 ** 2))
-
-        return Grad()
-
     def _run(self, algorithm):
-        
-        samples = []
+
+        xmin1 = -2.5
+        xmax1 = 0.0
+        xmin2 = 0.0
+        xmax2 = 2.5
+        p_occ = 0.382
+
         swapper = AlternatingAdjacentSwapScheme(algorithm)
 
-        for i in range(self.nits):
-            if i % 5 == 0 and i > 0:
-                swapper.swap_all()
-                samples.append(algorithm.state)
-
-            samples.append(algorithm.sample())
-
-        states = [x[0].position[1] for x in samples]
-
-        self.assertAlmostEqual(np.array(states).mean(), 0., delta=1.75e-1)
-        self.assertAlmostEqual(np.array(states).var(), 1., delta=1.75e-1)
-
-    def testReplicaExchangeMC(self):
+        n_occ1 = 0
+        n_occ2 = 0
         
-        algorithm = ReplicaExchangeMC(self.samplers, self.params)
+        for i in range(self.nits):
+            if i % 5 == 0:
+                swapper.swap_all()
+
+            else:
+                algorithm.sample()
+
+            x = self.samplers[0].state.position[0]
+            
+            if x > xmin1 and x < xmax1:
+                n_occ1 += 1
+            if x > xmin2 and x < xmax2:
+                n_occ2 += 1
+
+        p_occ_sampled1 = float(n_occ1) / float(self.nits)
+        p_occ_sampled2 = float(n_occ2) / float(self.nits)
+
+        # Assert by comparison with real occupation probabilities and a tolerance of
+        # four standard deviations of a run with n=15000 samples and 100 iterations
+        
+        self.assertAlmostEqual(p_occ_sampled1, p_occ, delta=4.0 * 0.035)
+        self.assertAlmostEqual(p_occ_sampled2, p_occ, delta=4.0 * 0.035)
+        
+    def testReplicaExchangeMC(self):
+
+        params = [RESwapParameterInfo(self.samplers[0], self.samplers[1])]
+        algorithm = ReplicaExchangeMC(self.samplers, params)
         self._run(algorithm)
 
     def testMDRENS(self):
-        
-        algorithm = MDRENS(self.samplers, self.params, FastLeapFrog)
+
+        self.set_1p_samplerparams()
+        params = [MDRENSSwapParameterInfo(self.samplers[0], self.samplers[1],
+                                          0.025, 15, self.grad)]
+        algorithm = MDRENS(self.samplers, params)
         self._run(algorithm)
 
     def testThermostattedMDRens(self):
-
-        algorithm = ThermostattedMDRENS(self.samplers, self.params, VelocityVerlet)
+        
+        self.set_1p_samplerparams()
+        params = [ThermostattedMDRENSSwapParameterInfo(self.samplers[0], self.samplers[1],
+                                                       0.05, 15, self.grad,
+                                                       temperature=self.Ts[0])]
+        algorithm = ThermostattedMDRENS(self.samplers, params)
         self._run(algorithm)
-        
-        
+
+    def testThermostattedMDRens_mm(self):
+        mm = np.array([[1., 0.], [0., 3.]])
+        self.set_2p_samplerparams()
+        params = [ThermostattedMDRENSSwapParameterInfo(self.samplers[0], self.samplers[1],
+                                                       0.025, 75, self.grad,
+                                                       temperature=self.Ts[0],
+                                                       mass_matrix=mm)]
+        algorithm = ThermostattedMDRENS(self.samplers, params)
+        self._run(algorithm)
+                
 if __name__ == '__main__':
 
     test.Console()
