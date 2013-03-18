5c9d5942421ee1543e8c3dd899906d8f51cdbfa8
simeoncarstens <simeoncarstens@localhost>
2013-03-18 15:57:11 +0100

    Implemented a functional test covering statistics.samplers.mc.singlechain.AbstractNCMCPropagator, statistics.samplers.mc.propagators.AbstractNCMCPropagator and statistics.samplers.mc.neqsteppropagator.MDPropagation

--- csb/test/cases/statistics/samplers/__init__.py
+++ csb/test/cases/statistics/samplers/__init__.py
@@ -12,8 +12,13 @@ from csb.statistics.samplers.mc.multichain import ThermostattedMDRENSSwapParamet
 from csb.statistics.samplers.mc.multichain import RESwapParameterInfo, AlternatingAdjacentSwapScheme
 from csb.statistics.samplers.mc.multichain import ReplicaExchangeMC, ThermostattedMDRENS
 from csb.statistics.samplers.mc.multichain import HMCStepRENS, HMCStepRENSSwapParameterInfo
-from csb.statistics.samplers.mc.singlechain import HMCSampler, RWMCSampler
+from csb.statistics.samplers.mc.singlechain import HMCSampler, RWMCSampler, AbstractNCMCSampler
 from csb.statistics.samplers.mc.propagators import RWMCPropagator, HMCPropagator
+from csb.statistics.samplers.mc.propagators import AbstractNCMCPropagator
+from csb.statistics.samplers.mc.neqsteppropagator import ReducedHamiltonian, HamiltonianSysInfo
+from csb.statistics.samplers.mc.neqsteppropagator import MDPropagation, MDPropagationParam
+from csb.statistics.samplers.mc.neqsteppropagator import Protocol, Step
+from csb.statistics.samplers.mc.neqsteppropagator import ReducedHamiltonianPerturbation
 
 class SamplePDF(Normal):
     
@@ -21,7 +26,7 @@ class SamplePDF(Normal):
         return sum(map(super(SamplePDF, self).log_prob, x))
 
     def grad(self, x, t):
-        return x
+        return x / (self.sigma ** 2)
 
 class MultimodalPDF(AbstractDensity):
 
@@ -98,6 +103,60 @@ class TestMCPropagators(test.Case):
         gen = HMCPropagator(self.pdf, self.gradient, self.timestep * 1.5, self.nsteps, mass_matrix=mm)
 
         self.checkResult(gen.generate(init_state, self.nits))
+
+    def testNCMCPropagator(self):
+
+        Nhalf = 30
+        dt = 0.3
+        md_tl = 1
+        
+        ks = np.linspace(1.0, 0.2, Nhalf).tolist()
+        sigmas = [1/np.sqrt(k) for k in ks]
+        sigmas += sigmas[::-1][1:]
+        N = len(sigmas)
+        pdfs = [SamplePDF(sigma=s) for s in sigmas]
+        hamiltonians = [ReducedHamiltonian(pdfs[i].log_prob, pdfs[i].grad) for i in range(N)]
+        sys_infos = [HamiltonianSysInfo(hamiltonians[i]) for i in range(N)]
+        
+        steps = [Step(ReducedHamiltonianPerturbation(sys_infos[i], sys_infos[i+1],
+                                                     evaluate_work=False),
+                      MDPropagation(sys_infos[i+1], MDPropagationParam(dt, md_tl, pdfs[i+1].grad),
+                                    evaluate_heat=False))
+                      for i in range(N - 1)]
+        rv_steps = [Step(ReducedHamiltonianPerturbation(sys_infos[i], sys_infos[i+1],
+                                                        evaluate_work=False),
+                         MDPropagation(sys_infos[i], MDPropagationParam(dt, md_tl, pdfs[i].grad),
+                                       evaluate_heat=False))
+                   for i in range(N - 1)]
+        
+        for s in rv_steps:
+            s.set_propagation_first()
+        protocol = Protocol(steps)
+        rv_protocol = Protocol(rv_steps)
+
+        class MDProbStepNCMCSampler(AbstractNCMCSampler):
+            def _calc_pacc(self, proposal_communicator):
+                return np.exp(-proposal_communicator.traj.deltaH)
+
+        class MDPropStepNCMCPropagator(AbstractNCMCPropagator):
+            def _init_sampler(self, init_state):
+                self._sampler = MDProbStepNCMCSampler(init_state, self.protocol,
+                                                      self.reverse_protocol)
+
+        gen = MDPropStepNCMCPropagator(protocol, rv_protocol)
+
+        init_state = State(np.array([1.0]), np.array([-0.5]))
+        
+        traj = gen.generate(init_state, self.nits, return_trajectory=True)
+
+        self.checkResult(traj)
+
+        self.assertAlmostEqual(np.mean([x.momentum[0] for x in traj]), 0.0, delta=0.15)
+        self.assertAlmostEqual(np.std([x.momentum[0] for x in traj]), 1.0, delta=0.15)
+
+
+
+
         
 @test.functional
 class TestMultichain(test.Case):
