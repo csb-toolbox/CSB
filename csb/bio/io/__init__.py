604eb0b3b0cfb2cf4c747d588af76a6c37dffa61
Ivan Kalev <ivan-kalev@users.noreply.github.com>
2013-11-04 17:39:39 +0100

    Refactoring: Remove deprecated modules, objects and methods

--- csb/bio/io/__init__.py
+++ csb/bio/io/__init__.py
@@ -7,7 +7,6 @@ from csb.bio.io.hhpred import HHpredOutputParser, HHpredProfileParser
 from csb.bio.io.clans import ClansParser, ClansFileWriter
 from csb.bio.io.wwpdb import StructureParser, AsyncStructureParser, PDBHeaderParser
 from csb.bio.io.fasta import SequenceParser, PDBSequenceParser
-from csb.bio.io.isites import ISitesParser
 from csb.bio.io.dssp import DSSPParser, StrideParser
 
 __all__ = ['HHOutputParser', 'HHProfileParser', 'ClansParser', 
